28476b179a35db26dfdb06d624c9d5ef659ac9c8
Marian Posaceanu <contact@marianposaceanu.com>
2017-03-31 17:39:21 +0300

    Updated seeds and yml file - #1

--- db/migrate/20170330225856_create_roles.rb
+++ db/migrate/20170330225856_create_roles.rb
@@ -2,7 +2,7 @@ class CreateRoles < ActiveRecord::Migration[5.0]
   def change
     create_table :roles do |t|
       t.string :name
-      t.string :activities, array: true, using: 'gin', default: '{}'
+      t.string :activities, array: true, default: []
       t.integer :user_id
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Article.create(title: 'This is an article', content: 'Hehehehe')
-Article.create(title: 'Not is an article', content: 'Hehehehe')
-Article.create(title: 'May is an article', content: 'Hehehehe')
-Article.create(title: 'Hay is an article', content: 'Hehehehe')
-Article.create(title: 'Way is an article', content: 'Hehehehe')
-Article.create(title: 'Tey is an article', content: 'Hehehehe')
-Article.create(title: 'Wake is an article', content: 'Hehehehe')
 
-admin_activities = [
-  'article:index',
-  'article:show',
-  'article:new',
-  'article:edit',
-  'article:create',
-  'article:update',
-  'article:destroy'
-]
+(1..10).each do
+  Article.create(title: Faker::TwinPeaks.quote, content: Faker::Lorem.paragraph)
+end
 
-simpleton_activities = [
-  'article:index',
-  'article:show'
-]
+activities = YAML.load_file("#{Rails.root}/config/activities.yml")
+
+admin_activities =
+    activities['article']['view'].map { |a| a['action'] } +
+    activities['article']['edit'].map { |a| a['action'] } +
+    activities['article']['destroy'].map { |a| a['action'] }
+
+simpleton_activities =
+    activities['article']['view'].map { |a| a['action'] }
 
 Role.create(name: 'admin', activities: admin_activities)
 Role.create(name: 'simpleton', activities: simpleton_activities)
